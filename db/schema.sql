5095310b9d06ff6c2b6df2ba80cf9ec9daf6286c
Sandy Freihofer <sandra.freihofer@gmail.com>
2010-12-09 17:49:21 +0100

    DB: change schema for winecellars
    Signed-off-by: Sandy Freihofer <sandra.freihofer@gmail.com>

--- db/schema.sql
+++ db/schema.sql
@@ -45,6 +45,7 @@ CREATE TABLE IF NOT EXISTS `winecellars` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `name` varchar(30) NOT NULL,
+  `capacity` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
